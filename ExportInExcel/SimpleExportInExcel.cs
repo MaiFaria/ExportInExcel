c97ce8239e42ffa4befa9fd035dd1839346a64b5
Maiara Faria <maiara.santos@theos.com.br>
2022-05-10 09:46:15 -0300

    First Commit - Método inicial para converter um List em IXLTable, gerar um Base64 e exportar em .xlsx.

---
+++ ExportInExcel/SimpleExportInExcel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ClosedXML.Excel;
+
+namespace ExportInExcel
+{
+    public static class SimpleExportInExcel
+    {
+        public static string ExportInExcel<T>(List<T> list, string sheetName)
+        {
+            using (IXLWorkbook wb = new XLWorkbook())
+            {
+                var workbook = wb.AddWorksheet(sheetName).FirstCell().InsertTable<T>(list, false);
+                workbook.Rows(1, 1).Style.Font.Bold = true;
+
+                using (var stream = new MemoryStream())
+                {
+                    wb.SaveAs(stream);
+
+                    return Convert.ToBase64String(stream.ToArray());
+                }
+            }
+        }
+    }
+}
